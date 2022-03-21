d71c008ed0ee5484d8c417f2fb3e032ae2053c3e
Roman <mmm.romka@gmail.com>
2022-03-21 14:26:58 +0000

    add basic_auth

--- k8s/4_run_nginx_ingress.sh
+++ k8s/4_run_nginx_ingress.sh
@@ -3,11 +3,10 @@
 ####Preparing
 
 echo '1: Load App ingress '
-kubectl create -f nginx_ingress/app_ingress.yaml
+kubectl create -f nginx_ingress/app_ingress_base_auth.yaml
 sleep 2
 
 echo '1: Load Prometheus ingress'
-kubectl create -f nginx_ingress/prom_ingress.yaml
-
+kubectl create -f nginx_ingress/prom_ingress_base_auth.yaml
 sleep 2
 
--- k8s/5_del_nginx_ingress.sh
+++ k8s/5_del_nginx_ingress.sh
@@ -4,12 +4,14 @@
 
 echo "Destroy Ingress"
 
-kubectl delete -f nginx_ingress/prom_ingress.yaml
+kubectl delete -f nginx_ingress/prom_ingress_base_auth.yaml
 
-kubectl delete -f nginx_ingress/app_ingress.yaml
+kubectl delete -f nginx_ingress/app_ingress_base_auth.yaml
 sleep 5
 
 kubectl delete -f nginx_ingress/nginx_ing_v2.yaml
+
+kubectl delete -f nginx_ingress/nginx_secret.yaml
 
 kubectl get pods 
 
